b4cb518cf7b8afaab05d2f87e48d8d5f2dcc1c1e
sem-git <lsmsm0327@naver.com>
2023-07-22 22:01:00 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import MyComponent from "./MyComponent";
+import ConfirmButton from "./ConfirmButtom";
+import Toolbar from "./Toolbar";
+import LandingPage from "./LandingPage";
 
 function App() {
   return (
     <div>
-      <MyComponent name={3}>리액트</MyComponent>
+      <LandingPage />
     </div>
   );
 }
